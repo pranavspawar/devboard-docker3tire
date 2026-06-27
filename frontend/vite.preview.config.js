1de3ad33edcf54985c28ad4bbcd2e4f8852944bc
Pranav Pawar <pranavpawar1001@gmail.com>
2026-06-27 17:50:07 +0530

    changes in vite config file

--- frontend/vite.preview.config.js
+++ frontend/vite.preview.config.js
@@ -2,7 +2,7 @@ export default {
   preview: {
     proxy: {
       '/api': {
-        target: 'http://backend:8080',
+        target: 'http://backend:8081',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
